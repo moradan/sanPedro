ccaecfaeeeebbe4f9334443007b693b1c183893d
Moradan <66131055+moradan@users.noreply.github.com>
2022-08-06 18:57:49 -0300

    Initial commit

---
+++ src/crypto/Cryptographer.java
@@ -0,0 +1,130 @@
+package crypto;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Cryptographer {
+    // Constants
+    final private int KEY_LENGTH = 16;
+    final private int IV_LENGTH = 16;
+    final private int SALT_LENGTH = 16;
+    final private int BITS_PER_BYTE = 8;
+    final private int MATERIALS_LENGTH = SALT_LENGTH + IV_LENGTH;
+    
+    // Fields
+    private boolean encrypt;
+    private byte[] salt;
+    private SecretKey key;
+    private IvParameterSpec iv;
+    private byte[] text;
+    private byte[] encodedMaterials = new byte[MATERIALS_LENGTH];
+    private byte[] output;
+
+    // Constructor
+    public Cryptographer(String password, byte[] text, boolean encrypt) {
+        this.encrypt = encrypt;
+        this.text = text;
+
+        if (this.encrypt) {
+            this.salt = generateSalt();
+            this.iv = generateIv();
+            this.encodeMaterials();
+        } else {
+            this.parseMaterials();
+        }
+        
+        try {
+            this.key = deriveKey(password, salt);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(-1);
+        }
+    }
+
+    // The following three methods generate the secrets fro the encryption
+    private IvParameterSpec generateIv() {
+        byte[] iv = new byte[IV_LENGTH];
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(iv);
+        return new IvParameterSpec(iv);
+    }
+
+    private byte[] generateSalt() {
+        byte[] salt = new byte[SALT_LENGTH];
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(salt);
+        return salt;
+    }
+
+    private SecretKey deriveKey(String password, byte[] salt) throws InvalidKeySpecException, NoSuchAlgorithmException {
+        final String DERIVE_ALGORITHM = "PBKDF2WithHmacSHA256";
+        final String CIPHER_ALGORITHM = "AES";
+        final int ITERATION_COUNT = 1024;
+
+        KeySpec specs = new PBEKeySpec(password.toCharArray(), salt, ITERATION_COUNT, KEY_LENGTH * BITS_PER_BYTE);
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DERIVE_ALGORITHM);
+        SecretKey key = keyFactory.generateSecret(specs);
+
+        return new SecretKeySpec(key.getEncoded(), CIPHER_ALGORITHM);
+    }
+
+    // Parse retrieves the required secrets from the beginning of the ciphertext.
+    private void parseMaterials() {
+        this.encodedMaterials = new byte[MATERIALS_LENGTH];
+
+        this.encodedMaterials = Arrays.copyOf(this.text, MATERIALS_LENGTH);
+
+        this.salt = Arrays.copyOfRange(this.encodedMaterials, 0, SALT_LENGTH);
+
+        this.iv = new IvParameterSpec(Arrays.copyOfRange(this.encodedMaterials, SALT_LENGTH, MATERIALS_LENGTH));
+    }
+
+    // Encode stores the required secrets into the beginning of the ciphertext.
+    private void encodeMaterials() {
+        System.arraycopy(this.salt, 0, this.encodedMaterials, 0, SALT_LENGTH);
+        System.arraycopy(this.iv.getIV(), 0, this.encodedMaterials, SALT_LENGTH, IV_LENGTH );
+    }
+
+    /* Processes the file either encrypting or decrypting it depending on the way the cryptographer object
+    was constructed.*/
+    public void processCipher() {
+        final String ALGORITHM = "AES/CBC/PKCS5Padding";
+
+        try {
+            byte[] buffer;
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            
+            if (this.encrypt) {
+                cipher.init(Cipher.ENCRYPT_MODE, this.key, this.iv);
+                buffer = cipher.doFinal(this.text);
+                this.output = new byte[MATERIALS_LENGTH + buffer.length];
+                
+                // Store encoded salt and iv, together with the cipher into the output array.
+                System.arraycopy(this.encodedMaterials, 0, this.output, 0, MATERIALS_LENGTH);
+                System.arraycopy(buffer, 0, this.output, MATERIALS_LENGTH, buffer.length);
+            } else {
+                cipher.init(Cipher.DECRYPT_MODE, this.key, this.iv);
+                buffer = Arrays.copyOfRange(this.text, MATERIALS_LENGTH, text.length);
+                this.output = cipher.doFinal(buffer);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(-1);
+        }
+    }
+
+    // Getter for the output field.
+    public byte[] getOutput() {
+        return this.output;
+    }
+}
---
+++ src/sanPedro.java
@@ -0,0 +1,94 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import crypto.Cryptographer;
+
+public class sanPedro {
+    public static void main(String[] args) {
+        long startTime;
+        long endTime;
+        long elapsed;
+        Scanner teclado = new Scanner(System.in);
+        String password;
+        String mode;
+        boolean encrypt = false;
+        byte[] text;
+        String fileName;
+        File sourceFile;
+        File targetFile;
+
+        // Request password
+        System.out.print("Password: ");
+        password = teclado.nextLine();
+
+        // Request mode
+        System.out.print("Encrypt or decrypt (e/d): ");
+        mode = teclado.nextLine();
+        if (mode.toLowerCase().equals("e")) {
+            encrypt = true;
+        } else if (!mode.toLowerCase().equals("d")) {
+            // if mode is not 'e' and it is not 'd' either application exits
+            System.out.println("Program terminating.");
+            System.exit(-1);
+        }
+
+        // Request source file
+        while (true) {
+            System.out.print("Origin file name: ");
+            fileName = teclado.nextLine();
+            sourceFile = new File(fileName);
+            if (!sourceFile.isFile()) {
+                System.out.println("No such file.");
+            } else break;
+        }
+
+        // Request target file
+        while (true) {
+            System.out.print("Target file name: ");
+            fileName = teclado.nextLine();
+            targetFile = new File(fileName);
+            if (targetFile.isFile()) {
+                System.out.print("The target file already exists. Would you like to overwrite it? y/n: ");
+                if (teclado.nextLine().toLowerCase().equals("y")) {
+                    break;
+                } else if (teclado.nextLine().toLowerCase().equals("n")) {
+                    continue;
+                }
+            } else break;
+        }
+
+        // Release input stream
+        teclado.close();
+
+        // Read source file to byte[]
+        try {
+            text = Files.readAllBytes(Paths.get(sourceFile.getPath()));
+            
+            // Init Cryptographer with passord, file file text byte[], encrypt boolean
+            Cryptographer cryptographer = new Cryptographer(password, text, encrypt);
+
+            // Process Cryptographer and measure time elapsed
+            startTime = System.currentTimeMillis();
+            cryptographer.processCipher();
+            endTime = System.currentTimeMillis();
+            elapsed = endTime - startTime;
+            System.out.println("Encryption/Decryption done in: " + elapsed + " miliseconds");
+
+
+            // Write Cryptographer.output to target file
+            startTime = System.currentTimeMillis();
+            Files.write(Paths.get(targetFile.getPath()), cryptographer.getOutput());
+            endTime = System.currentTimeMillis();
+            elapsed = endTime - startTime;
+            System.out.println("File was written in: " + elapsed + " miliseconds");
+
+        } catch (OutOfMemoryError | SecurityException | IOException e) {
+            System.out.println(e.getMessage());
+            System.exit(-1);
+        }
+
+    }
+}
